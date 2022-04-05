3be6084c3d89052d71278367b7ae3dcb88706988
ankkalampi <paul.veltheim@gmail.com>
2022-04-06 00:27:58 +0300

    ikkunan luominen

--- src/logic/Runtime.py
+++ src/logic/Runtime.py
@@ -1,2 +1,27 @@
 import pygame
+from pygame.locals import *
+ 
+pygame.init()
+vec = pygame.math.Vector2  # 2 for two dimensional
+ 
+HEIGHT = 450
+WIDTH = 400
+ACC = 0.5
+FRIC = -0.12
+FPS = 60
+ 
+FramePerSec = pygame.time.Clock()
+ 
+displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Game")
 
+run = True
+
+while run:
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
